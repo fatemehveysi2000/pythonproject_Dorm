abd064c88be05946a54f6efe2ac4efc7f0977e66
fafa <fatemehveysi1378@gmail.com>
2020-03-23 13:56:29 -0700

    :)

--- Dormitory.py
+++ Dormitory.py
@@ -1,30 +1,24 @@
 from tkinter import *
+from _tkinter import *
 import tkinter.ttk as ttk
 import tkinter.messagebox as tkMessageBox
 import sqlite3
-
 #------------------------------------------- Class -----------------------------------
-array = []
+array_employee = []
 # =============================(CLASSES)==========================
-
 class Employee:
-
     def __init__(self, name, family, roll, personal_number):
         self.name = name
         self.family = family
         self.roll = roll
         self.personal_number = personal_number
-
-
 class Student:
-
     def __init__(self, name, family, field, student_number):
         self.name = name
         self.family = family
         self.field = field
         self.student_number = student_number
 class Room:
-
     def __init__(self, number, block_name, floor, students):
         self.number = number
         self.block_name = block_name
@@ -36,9 +30,7 @@ class Block:
         self.block_name = block_name
         self.superviser = supervisor
         self.rooms = rooms
-
 #-------------------------- pages ------------------------------------
-
 class Main_Page:
     def __init__ (self, root):
         self.root=root
@@ -46,30 +38,66 @@ class Main_Page:
         self.root.geometry("600x600")
         self.root.title("Main Page")
 
-        BlockBTN = Button(self.root, text='Block', width="50", height="2", bd='3', activebackground='green', bg='white')
-        EmployeeBTN = Button(self.root, text='Employee', width="50", height="2", bd='3', activebackground='#0000ff', bg='white')
-        SrearchBTN = Button(self.root, text='Search', width='50', height='2', bd='3', activebackground='#ff8000', bg='white')
 
-        label = Label(self.root, text='Welcome Boss!', font=("Courier", 24 ))
-        label.place(x='10', y='10')
+
+
+        BlockBTN = Button(self.root, text='Block', width="10", height="1", bd='3', activebackground='green', bg='white')
+        EmployeeBTN = Button(self.root, text='Employee', width="10", height="1", bd='3', activebackground='#0000ff', bg='white',
+                             command=lambda: rabet(employee_menu, root))
+        SrearchBTN = Button(self.root, text='Search', width='10', height='1', bd='3', activebackground='#ff8000', bg='white')
+
+        label = Label(self.root, text='Welcome Boss!', font=("Courier", 12))
+        label.place(x='80', y='280')
 
         BlockBTN.pack(padx=5, pady=20)
         EmployeeBTN.pack(padx=5,pady=100)
         SrearchBTN.pack(padx=5,pady=100)
+        EmployeeBTN.pack(padx=5,pady=20)
+        SrearchBTN.pack(padx=5,pady=20)
+        self.root.geometry("%dx%d+%d+%d" % (300, 300, 500, 200))
+        self.root.resizable(0, 0)
 
-        SrearchBTN.mainloop()
-        BlockBTN.mainloop()
-        EmployeeBTN.mainloop()
-        self.root.mainloop()
+        # SrearchBTN.mainloop()
+        # BlockBTN.mainloop()
+        # EmployeeBTN.mainloop()
+        # self.root.mainloop()
 
-        self.root.bind()
+
+
+#----------------------------------منوی کارمندان-----------------------------------
+class employee_menu :
+        def __init__(self, root):
+            self.root = root
+            self.root.configure(bg="black")
+            self.root.geometry("600x600")
+            self.root.title("Emoloyee_Menu")
+
+            AddBTN = Button(self.root, text='Add', width="10", height="1", bd='3', activebackground='green',
+                              bg='white' ,  command=lambda: rabet(add_staff, root))
+            RemoveBTN = Button(self.root, text='Remove', width="10", height="1", bd='3', activebackground='#0000ff',
+                                 bg='white', command=lambda: rabet(remove_staff,root))
+            ShowList_BTN = Button(self.root, text='Show List', width='10', height='1', bd='3', activebackground='#ff8000',
+                                bg='white')
+
+
+                # SrearchBTN.mainloop()
+                # BlockBTN.mainloop()
+                # EmployeeBTN.mainloop()
+                # self.root.mainloop()
+
+            self.root.bind()
+            AddBTN.pack(padx=5, pady=20)
+            RemoveBTN.pack(padx=5, pady=20)
+            ShowList_BTN.pack(padx=5, pady=20)
+            self.root.geometry("%dx%d+%d+%d" % (300, 300, 500, 200))
+            self.root.resizable(0, 0)
+
 
 #----------------------------------
 #===============================================================
 class rabet :
 
     def __init__(self,_class,root):
-
        self.new=Toplevel(root)
        _class(self.new)
 # def Signup_menu_graphics():
@@ -99,35 +127,29 @@ class add_staff :
         self.root.geometry("%dx%d+%d+%d" % (500, 400, x, y))
         # self.root.resizable(0, 0)
 
+        self.root.geometry("%dx%d+%d+%d" % (500, 300, 500, 200))
+        self.root.resizable(0, 0)
+
         FIRSTNAME = StringVar()
         LASTNAME = StringVar()
         PERSONAL_NUMBER = StringVar()
         ROLL = StringVar()
-
         ContactForm = Frame(root)
-
         ContactForm.pack(side=TOP, pady=50 ,padx=100)
-
         lbl_firstname=Label(ContactForm, text="Firstname", font=('arial', 14), bd=5)
         lbl_firstname.grid(row=0, sticky=W)
-
         lbl_lastname = Label(ContactForm, text="Lastname", font=('arial', 14), bd=5)
         lbl_lastname.grid(row=1, sticky=W)
-
         lbl_roll = Label(ContactForm, text="Roll", font=('arial', 14), bd=5)
         lbl_roll.grid(row=3, sticky=W)
-
         lbl_personalnumber = Label(ContactForm, text="Personal Number", font=('arial', 14), bd=5)
         lbl_personalnumber.grid(row=5, sticky=W)
-
         firstname = Entry(ContactForm, textvariable=FIRSTNAME, font=('arial', 14))
         firstname.grid(row=0, column=1)
         lastname = Entry(ContactForm, textvariable=LASTNAME, font=('arial', 14))
         lastname.grid(row=1, column=1)
-
         roll = Entry(ContactForm, textvariable=ROLL, font=('arial', 14))
         roll.grid(row=3, column=1)
-
         personalnumber = Entry(ContactForm, textvariable=PERSONAL_NUMBER, font=('arial', 14))
         personalnumber.grid(row=5, column=1)
         btn_save = Button(ContactForm, text="Save", width=50,command=lambda: self.Save_staff(FIRSTNAME.get(),LASTNAME.get(),ROLL.get(),PERSONAL_NUMBER.get()))
@@ -135,14 +157,52 @@ class add_staff :
         ContactForm.pack()
     def Save_staff(self,name, family, roll,personalnmber):
 
-        array.append(Employee(name, family, roll,personalnmber))
+        array_employee.append(Employee(name, family, roll,personalnmber))
 #-------------------------------------------------------------------------------
+#----------------------------------- حذف کامندان --------------------------------------------
+
+class remove_staff:
+    def __init__(self,root):
+
+        self.root = root
+        self.root.title("REMOVE:staf")
+        self.root["bg"] = "black"
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        x = (screen_width / 2) - (100)
+        y = (screen_height / 2) - (200)
+        self.root.geometry("%dx%d+%d+%d" % (300, 500, x, y))
+        self.root.resizable(0, 0)
+
+        #         show list
+
+
+        inputNumber = IntVar()
+        lbl_input = Label(self.root, text="Enter number", font=('arial', 10), bd=5)
+        lbl_input.place(x=5,y=420)
+
+        input = Entry(self.root, textvariable=inputNumber,width=16, font=('arial', 14))
+        input.place(x=110,y=420)
+
+        staffReamoveBtn = Button(self.root, text="Remove", command=lambda: self.reamove_Staff(inputNumber), width=30)
+        staffReamoveBtn.pack(side=BOTTOM)
+
+    def reamove_Staff(self,num):
+
+         del array_employee[num.get()-1]
+         print(array_employee[num.get()-1].roll)
+
+
+
+
+
 
 
 
 #------------------------------ Main ------------------------------
 
 if __name__ == '__main__':
+
     root=Tk()
     first=Main_Page(root)
     root.mainloop()
