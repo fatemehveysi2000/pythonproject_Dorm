f90045719501541d6cb4a29cd991d0557393f644
YeganehGh97 <yeganehghani174@gmail.com>
2020-03-23 20:37:25 +0430

    Main page with no bug

--- Dormitory.py
+++ Dormitory.py
@@ -3,16 +3,16 @@ import tkinter
 
 class Main_Page:
     def __init__ (self, root):
-        MailPage = tkinter.Tk()
-        MailPage.configure(bg="black")
-        MailPage.geometry("600x600")
-        MailPage.title("Main Page")
+        self.root=root
+        self.root.configure(bg="black")
+        self.root.geometry("600x600")
+        self.root.title("Main Page")
 
-        BlockBTN = Button(MailPage, text='Block', width="50", height="2", bd='3', activebackground='green', bg='white')
-        EmployeeBTN = Button(MailPage, text='Employee', width="50", height="2", bd='3', activebackground='#0000ff', bg='white')
-        SrearchBTN = Button(MailPage, text='Search', width='50', height='2', bd='3', activebackground='#ff8000', bg='white')
+        BlockBTN = Button(self.root, text='Block', width="50", height="2", bd='3', activebackground='green', bg='white')
+        EmployeeBTN = Button(self.root, text='Employee', width="50", height="2", bd='3', activebackground='#0000ff', bg='white')
+        SrearchBTN = Button(self.root, text='Search', width='50', height='2', bd='3', activebackground='#ff8000', bg='white')
 
-        label = Label(MailPage, text='Welcome Boss!', font=("Courier", 24 ))
+        label = Label(self.root, text='Welcome Boss!', font=("Courier", 24 ))
         label.place(x='10', y='10')
 
         BlockBTN.pack(padx=5, pady=20)
@@ -27,6 +27,6 @@ class Main_Page:
         MailPage.bind()
 
 if __name__ == '__main__':
-    root=tkinter.Tk()
+    root=Tk()
     first=Main_Page(root)
     root.mainloop()
